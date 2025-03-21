17884eaba94b3b66da2673ed2632950f2d015352
Philippe Chassé <54691270+philippechasse@users.noreply.github.com>
2025-03-21 16:14:10 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,51 +1,36 @@
 const config = {
-  name: "Guillaume Levrier",
+  name: "Philippe Chassé",
   description: {
     EN: {
-      short: `Guillaume Levrier is a political scientist working on biotechnology. He is a research associate to <a
-            href="https://www.sciencespo.fr/cevipof/en/researcher/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> and to the French National Library (BnF). He is also the lead 
-            developer of <a href="https://github.com/Guillaume-Levrier/PANDORAE"target="_blank">PANDORÆ</a> and a 
-            member of the French <a href="https://www.ouvrirlascience.fr/software-and-source-codes-college/"target="_blank">Committee for Open Science</a>.`,
+      short: ``,
       long: "",
     },
     FR: {
-      short: `Guillaume Levrier est un chercheur en sciences
-        politiques
-        spécialisé sur les biotechnologies. Il est associé au <a
-            href="https://www.sciencespo.fr/cevipof/fr/chercheur/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> et à la Bibliothèque nationale de
-        France (BnF). Il est aussi l'architecte de <a href="https://github.com/Guillaume-Levrier/PANDORAE"target="_blank">
-        PANDORÆ</a> et membre du <a href="https://www.ouvrirlascience.fr/college-codes-sources-et-logiciels/"target="_blank">comité pour la science ouverte</a>.`,
+      short: `Philippe Chassé est candidat au doctorat en science politique à Sciences Po et à l’Université de Montréal. Il est diplômé de l’Université McGill (BA, MA), où il a été boursier Guy Drummond de 2018 à 2020. Dans le cadre de ses activités de recherche, il est affilié au Centre de recherches de Sciences Po (CEVIPOF) et au Centre pour l’étude de la citoyenneté démocratique (CECD). Il est dirigé par les professeurs Martial Foucault et Richard Nadeau. Ses recherches portent sur le lien entre les pratiques linguistiques et les attitudes politiques, le rôle du langage dans la formation de l’image des personnalités politiques et, plus généralement, les comportements politiques. Il s’intéresse également aux enjeux liés à la représentation politique. Sa thèse de doctorat se penche sur les attitudes des électeurs à l’égard du style langagier des personnes qui souhaitent obtenir leur vote. En se basant sur une approche expérimentale, elle vise à déterminer si la façon dont s’exprime un candidat politique peut avoir un impact sur l’évaluation que font les électeurs de sa capacité à les représenter. Ses travaux ont notamment été publiés dans Electoral Studies, Publius: The Journal of Federalism, The Canadian Journal of Political Science, French Politics, Canadian Political Science Review ainsi que dans la revue de sociolinguistique Glottopol. Philippe Chassé est titulaire d’un contrat doctoral ainsi que d’une bourse d’études supérieures du Canada Joseph-Armand-Bombardier (BESC D).`,
       long: "",
     },
   },
   links: [
     {
-      label: "twitter",
-      value: "@g_levrier",
-      url: "https://twitter.com/g_levrier",
+      label: "bluesky",
+      value: "@philippechasse",
+      url: "https://bsky.app/profile/philippechasse.bsky.social",
     },
     {
       label: "google scholar",
       value: "profile",
-      url: "https://scholar.google.com/citations?user=IRiJFNMAAAAJ&hl=en",
-    },
-    {
-      label: "pubpeer",
-      value: "profile",
-      url: "https://pubpeer.com/search?q=guillaume+levrier",
+      url: "https://scholar.google.com/citations?user=RPZbwYMAAAAJ&hl=fr&oi=ao",
     },
     {
       label: "orcid",
-      value: "3-4801-4770",
-      url: "https://orcid.org/0000-0003-4801-4770",
+      value: "2-3035-6206",
+      url: "https://orcid.org/0000-0002-3035-6206",
     },
     {
       label: "linked in",
       value: "profile",
-      url: "https://www.linkedin.com/in/guillaume-levrier-phd-89156838/",
+      url: "https://www.linkedin.com/in/philippe-chass%C3%A9-37090674/",
     },
   ],
-  email: { name: "guillaume.levrier", domain: "politique.science" },
+  email: { name: "philippe.chasse", domain: "sciencespo.fr" },
 };
